87161edfd30e42232600c5dd5d4b10b0e16f1f57
Fabio Antonio <fabio.antonnio@outlook.com>
2022-12-21 01:46:05 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,19 @@ namespace SalesWebMvc.Controllers
             return View(list);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
 
+        // INDICATE IS POST
+        [HttpPost]
+        // PREVINE ATACK XSRF/CSRF
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,5 +20,11 @@ namespace SalesWebMvc.Services
             // GET ALL SELLERS ON BD AND TURN INTO A LIST
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
