a23ec84dd50c7a2a69c70edb1db2a90bd3ed97df
Fabio Antonio <fabio.antonnio@outlook.com>
2022-12-20 22:08:53 -0300

    First Controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "My first page in MVC.";
 
             return View();
         }
